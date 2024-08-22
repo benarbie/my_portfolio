cafbdec3d633557db27fdf5901727975fec16724
benarbie <156953665+benarbie@users.noreply.github.com>
2024-08-22 10:36:43 +0300

    Added a csv file for visual, and fixed notebook

--- Adidas_Project/convert.py
+++ Adidas_Project/convert.py
@@ -1,11 +1,15 @@
-# Here i just export the dataset without the Adidas title
+# Here i just export the dataset without the Adidas title (The excel file has a visual Adidas title)
 
 import pandas as pd
 
-
-# Start reading from the 5th row, and skip the first empty column
+# Read the data, start reading from the 5th row
 df = pd.read_excel('Adidas US Sales Datasets.xlsx', skiprows=4)
+
+# Skip the first empty column reading from ['Retailer']
 df = df.loc[:, 'Retailer':]
 
+# Output the data
 df.to_excel('Adidas US Sales Datasets Updated.xlsx', index=False)
 
+# Convert to csv for visibility in Github
+df.to_csv('adidas_us_sales.csv', index=False)
